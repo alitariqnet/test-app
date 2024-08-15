a5917166ee5756419c32767e1f83dcac96ea66a9
Ali Tariq <alibabapk@aol.com>
2024-08-15 08:06:21 +0500

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import {Root, loader as rootLoader} from './routes/root';
+import ErrorPage from './error-page';
+import Contact from './routes/contact';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Root/>,
+    errorElement:<ErrorPage />,
+    loader: rootLoader,
+    children:[
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ]
+  },
+  
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
