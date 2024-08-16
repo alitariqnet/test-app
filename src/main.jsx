65fcac6135e3dd93fbaa968b94dc05fa25487086
Ali Tariq <alibabapk@aol.com>
2024-08-16 17:44:18 +0500

    more routes

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,8 @@ import Root, {action as rootAction, loader as rootLoader } from './routes/root';
 import ErrorPage from './error-page';
 import Contact, { loader as contactLoader } from './routes/contact'; 
 import EditContact, { action as editAction } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from './routes';
 
 const router = createBrowserRouter([
   {
@@ -18,6 +20,7 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
@@ -28,6 +31,11 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
       },
     ],
   },
